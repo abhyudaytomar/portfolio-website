dc36dc365a9fc490f6c0002c1cfd8cfa64c57625
Abhyuday <abhyudaytomar12@gmail.com>
2024-05-18 13:27:08 +0530

    minor changes

--- portfolio-website/src/Components/Footer/Footer.js
+++ portfolio-website/src/Components/Footer/Footer.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Container, Typography, Link } from '@mui/material';
-import './Footer.css'; 
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <Container maxWidth="lg">
-        <Typography variant="body1" align="center" color="textSecondary" gutterBottom>
-          Your footer content goes here.
-        </Typography>
-        <Typography variant="body2" align="center" color="textSecondary" component="p">
-          © {new Date().getFullYear()} Your Company Name
-        </Typography>
-        <Typography variant="body2" align="center" color="textSecondary" component="p">
-          Website designed and developed by <Link href="https://yourwebsite.com" color="inherit">Your Name</Link>
+        <Typography variant="body2" align="center"  component="p" className="footer-text">
+        Designed and Developed by Abhyuday Tomar © 2024 All Rights Reserved.
         </Typography>
       </Container>
     </footer>
